c4eda7518c4ba7b78840143f37ebc480dd3a34d9
partho <partho@maxotek.com>
2018-02-25 02:12:47 +0530

    Added init.script

--- README.md
+++ README.md
@@ -3,15 +3,28 @@ Seed project for creating AngularJS components, directives &amp; Services
 
 Clone this repository and then change the following entires:-
 
+# Manual Method
+
 **package.json**
-name - This is the name of your project
-description - Give your project a meaningful description
-repository - The URL to your Git repository
+1. name - This is the name of your project
+2. description - Give your project a meaningful description
+3. repository - The URL to your Git repository
 
 **bower.json**
-name - This is the name of your project
-description - Give your project a meaningful description
-homepage - The URL to your Git repository
+1. name - This is the name of your project
+2. description - Give your project a meaningful description
+3. homepage - The URL to your Git repository
 
 **build.conf.js**
-buildJsFilename - The compressed & minified name of your output JavaScript file. This is what others will include using <code>&lt;script&gt;</code> tags
+1. buildJsFilename - The compressed & minified name of your output JavaScript file. This is what others will include using <code>&lt;script&gt;</code> tags
+
+# Automated Approach
+Use the init.js script to automatically update these values.
+
+**Usage**
+
+<code>node init.js &lt;project-name&gt; &lt;project-description&gt; &lt;git-repository-url&gt; &lt;output-filename&gt;</code>
+
+**Example**
+
+<code>node init.js "your-project-name" "What your awesome project does" "https://github.com/you/your-project-name" "your-project-name.js"</code>
---
+++ init.js
@@ -0,0 +1,63 @@
+var fs = require('fs');
+
+// Verify that files exist
+var packageFile = "package.json";
+var bowerFile = "bower.json";
+var buildConfFile = "build.conf.js";
+
+var filesToCheck = [packageFile, bowerFile, buildConfFile];
+if (filesToCheck.some(checkFileMissing))
+    return -1;
+
+// Check Arguments
+if (process.argv.length < 6) {
+    console.error("Usage: <project-name> <project-description> <git-repository-url> <output-filename>");
+    return -1;
+}
+
+var projectName = process.argv[2];
+var projectDescription = process.argv[3];
+var repoUrl = process.argv[4];
+var outputFileName = process.argv[5];
+
+// Dump Arguments
+console.log("Project: " + projectName);
+console.log("Description: " + projectDescription);
+console.log("Repository URL: " + repoUrl);
+console.log("Output File: " + outputFileName);
+
+// Modify package.json
+var content = fs.readFileSync(packageFile)
+var obj = JSON.parse(content);
+obj.name = projectName;
+obj.description = projectDescription;
+obj.repository = repoUrl;
+fs.writeFileSync(packageFile, JSON.stringify(obj, null, 2));
+
+console.log(packageFile + " updated");
+
+// Modify bower.json
+var content = fs.readFileSync(bowerFile)
+var obj = JSON.parse(content);
+obj.name = projectName;
+obj.description = projectDescription;
+obj.homepage = repoUrl;
+fs.writeFileSync(bowerFile, JSON.stringify(obj, null, 4));
+
+console.log(bowerFile + " updated");
+
+// Modify build.conf.js
+var content = fs.readFileSync(buildConfFile).toString();
+content = content.replace(/buildJsFilename:(\s*)(['"])[^']+(['"])/, "buildJsFilename:$1$2" + outputFileName + "$3");
+fs.writeFileSync(buildConfFile, content);
+
+console.log(buildConfFile + " updated");
+
+function checkFileMissing(fileName) {
+    if (!fs.existsSync(fileName)) {
+        console.error(fileName + " does not exist");
+        return true;
+    }
+
+    return false;
+}
